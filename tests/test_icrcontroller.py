cc74a1b9f997e3c6d7cfe9d125c9203116a3e56d
James Ward <james@thedropbears.org.au>
2019-01-03 11:20:14 +1100

    Reformat tests with black

--- tests/test_icrcontroller.py
+++ tests/test_icrcontroller.py
@@ -3,13 +3,15 @@ import numpy as np
 
 
 def test_icrc_init():
-    icrc = Controller(np.array([0]),  # modules_alpha
-                         np.array([1]),  # modules_l
-                         np.array([0.1]),  # modules_b
-                         np.array([0.1]),  # modules_r
-                         np.zeros(shape=(3, 1)),  # epsilon_init
-                         [-1, 1],  # beta_bounds
-                         [-1, 1],  # beta_dot_bounds
-                         [-1, 1],  # beta_2dot_bounds
-                         [-1, 1],  # phi_dot_bounds
-                         [-1, 1])  # phi_2dot_bounds
+    icrc = Controller(
+        np.array([0]),  # modules_alpha
+        np.array([1]),  # modules_l
+        np.array([0.1]),  # modules_b
+        np.array([0.1]),  # modules_r
+        np.zeros(shape=(3, 1)),  # epsilon_init
+        [-1, 1],  # beta_bounds
+        [-1, 1],  # beta_dot_bounds
+        [-1, 1],  # beta_2dot_bounds
+        [-1, 1],  # phi_dot_bounds
+        [-1, 1],
+    )  # phi_2dot_bounds
--- tests/test_icrestimator.py
+++ tests/test_icrestimator.py
@@ -14,6 +14,7 @@ def init_icre(alphas, ls, bs):
     icre = Estimator(epsilon, alphas, ls, bs)
     return icre
 
+
 def test_estimate_lambda():
     icre = init_icre([0, math.pi / 2, math.pi], [1, 1, 1], [0, 0, 0])
 
@@ -24,12 +25,12 @@ def test_estimate_lambda():
 
     q = np.array([math.pi / 4, 0, -math.pi / 4])
     icr = np.array([0, -1, 1])
-    desired_lmda = icr * 1/np.linalg.norm(icr)
+    desired_lmda = icr * 1 / np.linalg.norm(icr)
     lmda_e = icre.estimate_lmda(q)
     assert np.allclose(desired_lmda, lmda_e.T, atol=tolerance)
 
     # driving along the y axis
-    q = np.array([0, math.pi/2, 0])
+    q = np.array([0, math.pi / 2, 0])
     # so the ICR should be on the U axis
     desired_lmda = np.array([1, 0, 0])
     lmda_e = icre.estimate_lmda(q)
@@ -82,12 +83,12 @@ def test_estimate_lambda():
 
 
 def test_joint_space_conversion():
-    icre = init_icre([math.pi/4], [1], [0])
+    icre = init_icre([math.pi / 4], [1], [0])
     lmda = np.array([0, 0, -1]).reshape(-1, 1)
     beta_target = np.array([0])
     assert np.allclose(beta_target, icre.S(lmda))
     lmda = np.array([0, -1, 0]).reshape(-1, 1)
-    beta_target = np.array([math.pi/4])
+    beta_target = np.array([math.pi / 4])
     assert np.allclose(beta_target, icre.S(lmda))
 
     # square robot with side length of 2 to make calculations simpler
@@ -110,53 +111,56 @@ def test_joint_space_conversion():
 
 def test_solve():
     # for now, check only for runtime errors until compute_derivatives works
-    icre = init_icre([math.pi/4, -math.pi/4, math.pi], [1, 1, 1], [0, 0, 0])
+    icre = init_icre([math.pi / 4, -math.pi / 4, math.pi], [1, 1, 1], [0, 0, 0])
     lmda = np.array([0, -1, 0]).reshape(-1, 1)
-    S_u = np.array([1/math.sqrt(2), 1/math.sqrt(2), 0])
+    S_u = np.array([1 / math.sqrt(2), 1 / math.sqrt(2), 0])
     S_v = np.array([0, 0, 1])
     q = np.array([0, 0, 0])
     icre.solve(S_u, S_v, q, lmda)
 
+
 def test_compute_derivatives():
     # for now, check only for runtime errors
-    icre = init_icre([math.pi/4, -math.pi/4, math.pi], [1, 1, 1], [0, 0, 0])
+    icre = init_icre([math.pi / 4, -math.pi / 4, math.pi], [1, 1, 1], [0, 0, 0])
     lmda = np.array([0, 0, -1]).reshape(-1, 1)
     S_u, S_v = icre.compute_derivatives(lmda)
 
+
 def test_handle_singularities():
-    icre = init_icre([0, math.pi/2, math.pi], [1, 1, 1], [0, 0, 0])
+    icre = init_icre([0, math.pi / 2, math.pi], [1, 1, 1], [0, 0, 0])
     # icr on wheel 0 on the R^2 plane
     icr = np.array([1, 0, 1]).reshape(-1, 1)
-    lmda = icr * 1/np.linalg.norm(icr)
+    lmda = icr * 1 / np.linalg.norm(icr)
     singularity, wheel_number = icre.handle_singularities(lmda)
     assert singularity
     assert wheel_number is 0
     icr = np.array([100, 0, 1]).reshape(-1, 1)
-    lmda = icr * 1/np.linalg.norm(icr)
+    lmda = icr * 1 / np.linalg.norm(icr)
     singularity, wheel_number = icre.handle_singularities(lmda)
     assert not singularity
     assert wheel_number is None
 
+
 def test_update_parameters():
-    icre = init_icre([0, math.pi/2, math.pi], [1, 1, 1], [0, 0, 0])
-    q = np.zeros(shape=(3,)) # ICR on the robot's origin
+    icre = init_icre([0, math.pi / 2, math.pi], [1, 1, 1], [0, 0, 0])
+    q = np.zeros(shape=(3,))  # ICR on the robot's origin
     desired_lmda = np.array([0, 0, 1])
-    u, v = -0.1, -0.1 # ICR estimate too negative
-    lmda_estimate = np.array([u, v,
-                              math.sqrt(1-np.linalg.norm([u, v]))]).reshape(-1, 1)
+    u, v = -0.1, -0.1  # ICR estimate too negative
+    lmda_estimate = np.array([u, v, math.sqrt(1 - np.linalg.norm([u, v]))]).reshape(
+        -1, 1
+    )
     delta_u, delta_v = 0.1, 0.1
-    lmda_t, worse = icre.update_parameters(lmda_estimate, delta_u, delta_v,
-                                           q)
+    lmda_t, worse = icre.update_parameters(lmda_estimate, delta_u, delta_v, q)
     assert np.allclose(lmda_t.T, desired_lmda)
     assert not worse
     delta_u, delta_v = -0.1, -0.1
-    lmda_t, worse = icre.update_parameters(lmda_estimate, delta_u, delta_v,
-                                           q)
+    lmda_t, worse = icre.update_parameters(lmda_estimate, delta_u, delta_v, q)
     assert worse
 
+
 def test_select_starting_points():
-    icre = init_icre([0, math.pi/2, math.pi], [1, 1, 1], [0, 0, 0])
-    q = np.zeros(shape=(3,)) # ICR on the robot's origin
+    icre = init_icre([0, math.pi / 2, math.pi], [1, 1, 1], [0, 0, 0])
+    q = np.zeros(shape=(3,))  # ICR on the robot's origin
     desired_lmda = np.array([0, 0, -1])
     starting_points = icre.select_starting_points(q)
     for sp in starting_points:
@@ -164,7 +168,7 @@ def test_select_starting_points():
 
     q = np.array([math.pi / 4, 0, -math.pi / 4])
     icr = np.array([0, -1, 1]).reshape(-1, 1)
-    desired_lmda = icr * 1/np.linalg.norm(icr)
+    desired_lmda = icr * 1 / np.linalg.norm(icr)
     starting_points = icre.select_starting_points(q)
     assert np.allclose(desired_lmda[:2], starting_points[0][:2])
     # should have unit norm
@@ -172,7 +176,7 @@ def test_select_starting_points():
         assert np.isclose(np.linalg.norm(sp), 1)
 
     # driving along the y axis
-    q = np.array([0, math.pi/2, 0])
+    q = np.array([0, math.pi / 2, 0])
     # so the ICR should be on the U axis
     desired_lmda = np.array([1, 0, 0]).reshape(-1, 1)
     starting_points = icre.select_starting_points(q)
@@ -191,7 +195,7 @@ def test_select_starting_points():
     q = np.array([6.429e-04, -6.429e-04, 3.1422, 3.1409])
     desired_lmda = np.array([0, 0, 1])
     sp = icre.select_starting_points(q)
-    close=[]
+    close = []
     for p in sp:
         close.append(np.allclose(desired_lmda, p.T, atol=tolerance))
     assert any(close)
